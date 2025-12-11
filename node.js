0ed9fc45b444b319619a10807a0433a3dea7207b
epiphanie <nyirambonigabaepiphany4@gmail.com>
2025-12-11 15:28:14 +0200

    Initial commit

---
+++ node.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+ if (req.url === '/About') {
+ res.statusCode = 200;
+ res.setHeader('Content-Type', 'text/plain');
+ res.end('About Page\n');
+ } else {
+ res.statusCode = 200;
+ res.setHeader('Content-Type', 'text/plain');
+ res.end('Hello, World!\n');
+ }
+});
+server.listen(3000, () => {
+ console.log('Server running at http://localhost:3000/');
+});
